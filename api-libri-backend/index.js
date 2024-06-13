a70336215a0a572d865a85ad13b1dd43bc23bfc7
kaah-kz <karolavila850@gmail.com>
2024-06-13 20:22:54 -0300

    ...

--- api-libri-backend/index.js
+++ api-libri-backend/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
+const cors = require ('cors');
 const routerLivro = require('./route/routesLivro');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.use('/', routerLivro);
 
-app.listen(3000, ()=>{
-    console.log('SERVIDOR RODANDO EM - http://localhost:3000');
+app.listen(5000, ()=>{
+    console.log('SERVIDOR RODANDO EM - http://localhost:5000');
 });
